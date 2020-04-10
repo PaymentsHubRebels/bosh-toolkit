a6beeab2c85d7812d27a061f2a85fd79357da176
Diego Fidalgo Moreno <dfidalgo@argotec.es>
2020-04-10 20:16:27 +0100

    first commit

---
+++ tasks/download-blobs/download_blobs.rb
@@ -0,0 +1,7 @@
+require 'yaml'
+
+blobs_config = YAML.load_file(ARGV[0])
+
+blobs_config.each do |blob_info|
+  `curl --create-dirs -o #{blob_info.fetch('bosh_blob_path')} #{blob_info.fetch('url')}`
+end
