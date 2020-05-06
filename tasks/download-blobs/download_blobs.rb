42476655b2fa0209c4be508bfae6e5a186843aad
Mehta Deepak <deepak.mehta@servexternos.gruposantander.com>
2020-05-06 13:41:26 +0100

    updated script to support ruby 2.5
    Signed-off-by: Gaurav Pal <gaur4v.pal@gmail.com>
    
    #172449957

--- tasks/download-blobs/download_blobs.rb
+++ tasks/download-blobs/download_blobs.rb
@@ -1,11 +1,12 @@
 require 'yaml'
+require 'set'
 
 blobs_config = YAML.load_file(ARGV[0])
 
-cachedBlobs= Dir.glob('./**/*').select{ |e| File.file? e }.map{ |f| f.reverse.chomp("./".reverse).reverse }
-requiredBLobs =[]
+cachedBlobs= Dir.glob('./**/*').select{ |e| File.file? e }.map{ |f| f.reverse.chomp("./".reverse).reverse }.to_set
+requiredBLobs = Set.new
 blobs_config.each { |item| requiredBLobs << item.fetch("bosh_blob_path") }
-unusedBlobs = cachedBlobs.difference(requiredBLobs)
+unusedBlobs = cachedBlobs.difference(requiredBLobs.to_set)
 unusedBlobs.each { |file | File.delete(file) if File.exist?(file) }
 
 blobs_config.each do |blob_info|
