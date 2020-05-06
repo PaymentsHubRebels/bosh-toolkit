29aedca0550e1cc31698f9a064e503fbc4898533
Mehta Deepak <deepak.mehta@servexternos.gruposantander.com>
2020-05-06 13:18:22 +0100

    removed method definition
    Signed-off-by: Gaurav Pal <gaur4v.pal@gmail.com>
    
    #172449957

--- tasks/download-blobs/download_blobs.rb
+++ tasks/download-blobs/download_blobs.rb
@@ -2,15 +2,11 @@ require 'yaml'
 
 blobs_config = YAML.load_file(ARGV[0])
 
-def delete_unused_cached_blobs
-  cachedBlobs= Dir.glob('./**/*').select{ |e| File.file? e }.map{ |f| f.reverse.chomp("./".reverse).reverse }
-  requiredBLobs =[]
-  blobs_config.each { |item| requiredBLobs << item.fetch("bosh_blob_path") }
-  unusedBlobs = cachedBlobs.difference(requiredBLobs)
-  unusedBlobs.each { |file | File.delete(file) if File.exist?(file) }
-end
-
-delete_unused_cached_blobs
+cachedBlobs= Dir.glob('./**/*').select{ |e| File.file? e }.map{ |f| f.reverse.chomp("./".reverse).reverse }
+requiredBLobs =[]
+blobs_config.each { |item| requiredBLobs << item.fetch("bosh_blob_path") }
+unusedBlobs = cachedBlobs.difference(requiredBLobs)
+unusedBlobs.each { |file | File.delete(file) if File.exist?(file) }
 
 blobs_config.each do |blob_info|
   `curl --create-dirs --location -o "#{blob_info.fetch('bosh_blob_path')}" "#{blob_info.fetch('url')}"` unless File.exist?("#{blob_info.fetch('bosh_blob_path')}")
