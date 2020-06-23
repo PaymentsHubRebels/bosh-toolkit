01af26d83908aaa40a012f08191585591fc1add1
Diego Fidalgo Moreno <Diego.FidalgoMoreno@santander.co.uk>
2020-06-23 15:59:26 +0100

    BOSH Release pipeline uploads only one compiled release
    Signed-off-by: Panagiotis Xynos <panagiotis.xynos@gruposantander.com>

--- tasks/export-release/release_version.rb
+++
@@ -1,8 +0,0 @@
-require "yaml"
-
-YAML.load_file(ENV["release_name"]+".yml")["releases"].each do |rel| 
-  if "#{rel["name"]}" == ENV["release_name"]
-    rel["version"]=ENV["target_version"] 
-  end
-puts "#{rel["name"]}/#{rel["version"]}"
-end
